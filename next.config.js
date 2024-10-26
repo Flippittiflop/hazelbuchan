debdbca9d161b7e077b7633ac99989af2d7f3ff2
jdtbuchan-ignite <jonathan@ignitesolutions.co.za>
2024-10-26 22:03:41 +0200

    Fixed nextjs config issues

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true, // Ensure trailing slashes for all routes
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
