6f0b4aa7d7f228637a9c2f2135f2e61593ec6fdf
jdtbuchan-ignite <jonathan@ignitesolutions.co.za>
2024-10-27 09:55:27 +0100

    Removed cypress for now

--- cypress/support/e2e.ts
+++
@@ -1,5 +0,0 @@
-// cypress/support/e2e.ts
-// This file is processed and loaded automatically before your test files.
-// You can use this file to set up global configuration and behavior that modifies Cypress.
-
-import './commands'; // Import custom commands if any
