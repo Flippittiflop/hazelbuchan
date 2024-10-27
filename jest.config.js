77ee5514a7f2437e90ea1e6741f34632851dad02
jdtbuchan-ignite <jonathan@ignitesolutions.co.za>
2024-10-27 09:44:56 +0100

    Changed jest.config file

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,7 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
+  testEnvironmentOptions: {
+    url: 'http://localhost'
+  }
 };
